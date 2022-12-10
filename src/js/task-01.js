0b7e851dff298a83451fc5185dc90e251802d922
Serhii <110534352+hakuna00723@users.noreply.github.com>
2022-12-10 14:09:51 +0100

    doing task1

---
+++ src/js/task-01.js
@@ -0,0 +1,13 @@
+console.log(
+  "Number of categories:",
+  document.getElementById("categories").getElementsByClassName("item").length
+);
+
+// const listItemsElements = document.querySelectorAll(".item");
+
+// console.log("Number of categories:", `${listItemsElements.length}`);
+
+// const allCategoriesArray = listItemsElements.forEach((el) => {
+//   console.log("Category:", `${el.querySelector("h2").textContent}`);
+//   console.log("Elements:", `${el.querySelectorAll("li").length}`);
+// });
