386083609561ff945fca360f583756be6584ac27
Serhii <110534352+hakuna00723@users.noreply.github.com>
2022-12-11 18:32:32 +0100

    task8-done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,15 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені");
+  }
+  console.log("Emeil: ", email.value);
+  console.log("Password: ", password.value);
+
+  event.currentTarget.reset();
+});
