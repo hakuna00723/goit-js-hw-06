5784cb7d77bc8494dc6840de88ca2bcfe30439c8
Serhii <110534352+hakuna00723@users.noreply.github.com>
2022-12-11 11:40:58 +0100

    task4-fixed

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,35 +1,15 @@
-// const counterValue = 0;
-
-// const minusBtn = document.querySelector('button[data-action="decrement"]');
-// minusBtn.addEventListener("click", () => {
-//   pullOutValue -= 1;
-//   function minusOne() {
-//     minusBtn.pullOutValue.textContent = counterValue;
-//   }
-// });
-
-// const pullOutValue = document.querySelector("#value");
-
-// const plusBtn = document.querySelector('button[data-action="increment"]');
-// plusBtn.addEventListener("click", (counterValue) => {
-//   counterValue += 1;
-//   plusBtn.pullOutValue.textContent = counterValue;
-// });
-
 let counterValue = 0;
 
-const btnCounter = {
-  counterIncrement: document.querySelector("[data-action='increment']"),
-  counterDecrement: document.querySelector("[data-action='decrement']"),
-  counterValueEl: document.querySelector("#value"),
-};
-
-btnCounter.counterIncrement.addEventListener("click", (onClickIncrement) => {
-  counterValue += 1;
-  btnCounter.counterValueEl.textContent = counterValue;
+const minusBtn = document.querySelector("[data-action='decrement']");
+minusBtn.addEventListener("click", (minusOne) => {
+  counterValue -= 1;
+  pullOutValue.textContent = counterValue;
 });
 
-btnCounter.counterDecrement.addEventListener("click", (onClickDecrement) => {
-  counterValue -= 1;
-  btnCounter.counterValueEl.textContent = counterValue;
+const pullOutValue = document.querySelector("#value");
+
+const plusBtn = document.querySelector("[data-action='increment']");
+plusBtn.addEventListener("click", (plusOne) => {
+  counterValue += 1;
+  pullOutValue.textContent = counterValue;
 });
