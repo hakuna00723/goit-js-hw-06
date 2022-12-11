bb2bd83a7768819adaff6e01cfeb1a4383acd12c
Serhii <110534352+hakuna00723@users.noreply.github.com>
2022-12-11 13:28:33 +0100

    task5-done

---
+++ src/js/task-05.js
@@ -0,0 +1,10 @@
+const elInput = document.querySelector("#name-input");
+const elOutput = document.querySelector("#name-output");
+
+elInput.addEventListener("input", (event) => {
+  elOutput.textContent = event.currentTarget.value;
+
+  if (elInput.value === "") {
+    return elOutput.textContent;
+  }
+});
