b02a1f60753dbf63d31acaec3f4b6b8f71dd6755
Serhii <110534352+hakuna00723@users.noreply.github.com>
2022-12-11 19:25:39 +0100

    task9-done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bcgr = document.querySelector("body");
+const txtColor = document.querySelector(".color");
+const btnChange = document.querySelector(".change-color");
+
+btnChange.addEventListener("click", () => {
+  let hexColor = getRandomHexColor();
+  bcgr.style.backgroundColor = hexColor;
+  txtColor.textContent = hexColor;
+});
